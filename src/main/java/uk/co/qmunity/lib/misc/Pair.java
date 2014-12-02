6d58052059d61ed2e36986ed7a7b4d26bcd3e686
Amadornes <amadornes@gmail.com>
2014-12-02 16:46:54 +0100

    Added Pairs (implementation the map Entry interface)

---
+++ src/main/java/uk/co/qmunity/lib/misc/Pair.java
@@ -0,0 +1,39 @@
+package uk.co.qmunity.lib.misc;
+
+import java.util.Map.Entry;
+
+public class Pair<K, V> implements Entry<K, V> {
+
+    private K key;
+    private V value;
+
+    public Pair(K key, V value) {
+
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public K getKey() {
+
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+
+        return value;
+    }
+
+    @Override
+    public V setValue(V value) {
+
+        return this.value = value;
+    }
+
+    public K setKey(K key) {
+
+        return this.key = key;
+    }
+
+}
