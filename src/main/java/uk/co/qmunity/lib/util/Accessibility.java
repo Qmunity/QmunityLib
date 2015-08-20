42ef140008125fd41c390b991e35c3546a636cbe
Amadornes <amadornes@gmail.com>
2015-08-20 10:30:18 +0200

    Moved a couple of things over from BP

---
+++ src/main/java/uk/co/qmunity/lib/util/Accessibility.java
@@ -0,0 +1,5 @@
+package uk.co.qmunity.lib.util;
+
+public enum Accessibility {
+    PUBLIC, SHARED, PRIVATE;
+}
---
+++ src/main/java/uk/co/qmunity/lib/util/IFace.java
@@ -0,0 +1,11 @@
+package uk.co.qmunity.lib.util;
+
+import net.minecraftforge.common.util.ForgeDirection;
+
+public interface IFace {
+
+    public void setFace(ForgeDirection dir);
+
+    public ForgeDirection getFace();
+
+}
